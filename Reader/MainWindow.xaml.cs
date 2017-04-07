7961e1cc782ab42db3792bbd269432ed1c5af3d2
Matthieu Wattecamps <Dies.Funus@gmail.com>
2017-04-07 17:46:21 +0200

    Ajoutez des fichiers projet.

---
+++ Reader/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ionic.Zip;
+using System.IO;
+using Ionic.Crc;
+
+namespace Reader
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+
+        }
+
+        Dictionary<int, BitmapImage> Pages = new Dictionary<int, BitmapImage>();
+
+
+            void CbzLoader(string CbzPath)
+        {
+
+            int i = 1;
+
+            using (ZipFile zip = ZipFile.Read(CbzPath))
+            {
+
+                foreach (ZipEntry e1 in zip)
+                {
+                    
+                    CrcCalculatorStream reader = e1.OpenReader();
+                    MemoryStream memstream = new MemoryStream();
+                    reader.CopyTo(memstream);
+                    byte[] bytes = memstream.ToArray();
+                    memstream.Seek(0, SeekOrigin.Begin);
+
+                    
+
+                    BitmapImage b = new BitmapImage();
+                    b.BeginInit();
+                    b.StreamSource = memstream;
+                    b.CacheOption = BitmapCacheOption.OnLoad;
+                    b.EndInit();
+                    InitializeComponent();
+
+                    Pages.Add(i, b);
+
+                    i++;
+
+                    //MainReader.Source = b;
+       
+                }
+            }
+        }
+        
+
+            void SinglePageViewer(int PageNumber)
+        {
+            
+            // Code à exécuter quand la méthode est appelée.
+            //BitmapImage b = new BitmapImage();
+            // b.BeginInit();
+            //b.UriSource = new Uri(Page);
+            //b.EndInit();
+            ///InitializeComponent();
+           // MainReader.Source = b;
+        }
+
+        private void Open_File_Event(object sender, MouseButtonEventArgs e)
+        {
+            // SinglePageViewer("C:\\002.png");
+            CbzLoader("c:\\test.cbz");
+            MainReader.Source = Pages[1];
+            MessageBox.Show("souris");
+           
+        }
+    }
+
+    
+}
+
