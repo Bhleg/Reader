16f06ae14617de28121111307da4611acc08eef5
Matthieu Wattecamps <Dies.Funus@gmail.com>
2017-04-14 00:15:04 +0200

    Rewrite of the ui

---
+++ Reader/FilePicker.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Reader
+{
+    /// <summary>
+    /// Interaction logic for File_Picker.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        string CurrentPath = "";
+        string DefaultPath = Environment.ExpandEnvironmentVariables("%HOMEPATH%");
+        List<Item> Items = new List<Item>();
+        //ObservableCollection<Library> LybraryCollection = new ObservableCollection<Library>();
+
+        public void File_Picker()
+        {
+            InitializeComponent();
+            FilePickerT.ItemsSource = Items;
+            GetContent(DefaultPath);
+            //GenerateLibrary();
+        }
+
+
+        //The Function wich is used to get the content of a folder(Path) and fill the Items list with the content of the folder(Path)
+        void GetContent(string Path)
+        {
+            //List<Item> Items = new List<Item>();
+            Items.Clear();
+
+
+
+            // Process the list of directory found in the directory.
+            string[] DirectoryEntries = Directory.GetDirectories(Path);
+            foreach (string DirectoriePath in DirectoryEntries)
+            {
+                string DirectorieName = System.IO.Path.GetFileName(DirectoriePath);
+                Items.Add(new Item() { Name = DirectorieName, Path = DirectoriePath, Type = "Folder", Icon = "/Icons/Folder.png" });
+            }
+
+            // Process the list of files found in the directory.
+            string[] FileEntries = Directory.GetFiles(Path).Where(s => s.EndsWith(".cbz") || s.EndsWith("cbr") || s.EndsWith("zip") || s.EndsWith("rar")).ToArray();
+            foreach (string FilePath in FileEntries)
+            {
+
+                string FileName = System.IO.Path.GetFileName(FilePath);
+                Items.Add(new Item() { Name = FileName, Path = FilePath, Type = "File", Icon = "/Icons/Message.png" });
+
+            }
+
+
+            CurrentPath = Path;
+
+        }
+
+
+        private void CreateLibrary()
+        {
+            Properties.Settings.Default.Library.Add(CurrentPath);
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+            string DirectorieName = System.IO.Path.GetFileName(CurrentPath);
+            string Command = "GetContent(" + CurrentPath + ")";
+
+            Menu.Add(new MenuPanelItem() { Name = DirectorieName, Command = CurrentPath });
+
+
+
+
+
+        }
+
+        
+
+        //Item class which is used for each entry inside the datagrid Filepicker
+        class Item
+        {
+            public string Name { get; set; }
+
+            public string Path { get; set; }
+
+            public string Type { get; set; }
+
+            public string Icon { get; set; }
+        }
+
+
+        //Library class which is used for each Library entry at the left of the datagrid Filepicker
+        [SettingsSerializeAs(SettingsSerializeAs.Xml)]
+        class Library
+        {
+            public string Name { get; set; }
+
+            public string Path { get; set; }
+
+            //public string Command { get; set; }
+
+
+        }
+
+
+
+        ////////////////////////////EVENT//////////////////////////////  
+
+        private void ButtonClick_Event(object sender, MouseButtonEventArgs e)
+        {
+            Item i = (Item)FilePickerT.SelectedItem;
+            String result = (i.Type).ToString();
+            if (result == "File")
+            {
+                //MainWindow m = Application.ReferenceEquals MainWindow();
+                MainWindow m = (MainWindow)Application.Current.MainWindow;
+                string Path = FilePickerT.SelectedValue.ToString();
+                //m.CbzLoader(Path);
+                m.FileLoader(Path);
+                //m.CbzLoader(FilePickerT.SelectedValue.ToString());
+                //FilePickerWindow.Hide();
+            }
+            else
+            {
+                GetContent(FilePickerT.SelectedValue.ToString());
+                FilePickerT.ItemsSource = Items;
+                FilePickerT.Items.Refresh();
+            }
+
+
+        }
+
+        //Event for the GoUP Button inside the File Picker Window
+        private void GoUp_Event(object sender, EventArgs e)
+        {
+
+
+            string GoUpPath = System.IO.Path.GetDirectoryName(CurrentPath);
+
+            if (GoUpPath == null)
+            {
+                GoUpPath = CurrentPath;
+
+            }
+            else
+            {
+
+                GetContent(GoUpPath);
+                FilePickerT.ItemsSource = Items;
+                FilePickerT.Items.Refresh();
+
+            }
+
+
+
+        }
+
+        //Event for the CreateLibrary Button inside the File Picker Window
+        protected void CreateLibrary_Event(object sender, EventArgs e)
+        {
+            //Button bn = sender as Button;
+            //GetContent(bn.Tag.ToString());
+            CreateLibrary();
+            //FilePickerT.ItemsSource = Items;
+            //FilePickerT.Items.Refresh();
+        }
+
+        //Event for the DeleteLibrary Button inside the File Picker Window
+        protected void PickerSettings_Event(object sender, EventArgs e)
+        {
+
+           DeleteLibraryGrid.ItemsSource = Menu;
+
+
+            //Button bn = sender as Button;
+           // GetContent(bn.Tag.ToString());
+            //FilePickerT.ItemsSource = Items;
+            //FilePickerT.Items.Refresh();
+        }
+
+        //Event for the Library's Button inside the File Picker Window
+        protected void GoToLibrary_Event(object sender, EventArgs e)
+        {
+            Button bn = sender as Button;
+            GetContent(bn.Tag.ToString());
+            FilePickerT.ItemsSource = Items;
+            FilePickerT.Items.Refresh();
+        }
+
+        //Event for the DeleteLibrary Button inside the File Picker Window
+        protected void DeleteLibrary_Event(object sender, EventArgs e)
+        {
+            Button bn = sender as Button;
+            Properties.Settings.Default.Library.Remove(bn.Tag.ToString());
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+            //DeleteLibraryGrid.Items.Refresh();
+            //Menu.Clear();
+            //MenuPanel();
+            
+
+
+            // Button bn = sender as Button;
+            // GetContent(bn.Tag.ToString());
+            // FilePickerT.ItemsSource = Items;
+            // FilePickerT.Items.Refresh();
+        }
+    }
+}
--- Reader/MainWindow.xaml.cs
+++ Reader/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.IO;
 using SharpCompress.Archives;
+using System.Windows.Controls;
 
 namespace Reader
 {
@@ -14,12 +15,20 @@ namespace Reader
     public partial class MainWindow : Window
     {
         
+        public MainWindow ()
+        {
+            InitializeComponent();
+            MenuPanel();
+            File_Picker();
+        }
         int CurrentPage = 0;
         int TotalPages = 0;
         string ViewerType = "Double";
         //string ViewerType = Properties.Settings.Default.DefaultViewer;
         // Dictionary<int, BitmapImage> Pages = new Dictionary<int, BitmapImage>();
         Dictionary<int, byte[]> Pages = new Dictionary<int, byte[]>();
+        List<MenuPanelItem> Menu = new List<MenuPanelItem>();
+        
 
         //Load a File (cbz or cbr), and put all of their relevant entry in a bitmapimage then in a Dictionary
         public void FileLoader(string FilePath)
@@ -53,13 +62,51 @@ namespace Reader
             }
             archive = null;
             TotalPages = i;
+            Viewer(ViewerType, "Start");
+            ShowReader();
 
-           // archive = null;
-           Viewer(ViewerType, "Start");
-            
+
         }
 
-        
+        public void MenuPanel()
+        {
+            
+            Menu.Add(new MenuPanelItem() { Name = "Explorer", Command = "Explorer", Icon = "/Icons/Folder.png" });
+            Menu.Add(new MenuPanelItem() { Name = "Reader",  Command = "Reader", Icon = "/Icons/Book.png" });
+            Menu.Add(new MenuPanelItem() { Name = "setting", Command = "Setting", Icon = "/Icons/Settings.png" });
+            GenerateLibrary();
+            
+            MenuItem.ItemsSource = Menu;
+            //MenuIcon.ItemsSource = Menu;
+            InitializeComponent();
+
+
+
+        }
+
+        void GenerateLibrary()
+        {
+            // BookmarkPanel.Children.Add(new Button { Content = "Button" });
+            foreach (string item in Properties.Settings.Default.Library)
+            {
+                string DirectorieName = System.IO.Path.GetFileName(item);
+                //string Command = "GetContent(" + item + ")";
+
+                Menu.Add(new MenuPanelItem() { Name = DirectorieName, Command = item, Type = "Library", Icon = "/Icons/Library.png" });
+            }
+
+        }
+
+        class MenuPanelItem
+        {
+            public string Name { get; set; }
+            public string Command { get; set; }
+            public string Type { get; set; }
+            public string Icon { get; set; }
+
+
+        }
+
         void Viewer(string ViewerType,string Action, int Page = 0)
         {
               
@@ -80,6 +127,7 @@ namespace Reader
                 if (z == "Load")
                 {
                     i = 1;
+
                 }
                 else if (z == "Next")
                 {
@@ -233,6 +281,27 @@ namespace Reader
 
         }
 
+        void ShowExplorer()
+        {
+            Settings.Visibility = Visibility.Collapsed;
+            Reader.Visibility = Visibility.Collapsed;
+            FilePicker.Visibility = Visibility.Visible;
+        }
+        void ShowReader()
+        {
+            Settings.Visibility = Visibility.Collapsed;
+            FilePicker.Visibility = Visibility.Collapsed;
+            Reader.Visibility = Visibility.Visible;
+        }
+        void ShowSetting()
+        {
+            FilePicker.Visibility = Visibility.Collapsed;
+            Reader.Visibility = Visibility.Collapsed;
+            Settings.Visibility = Visibility.Visible;
+        }
+        ////////////////////////////EVENT//////////////////////////////  
+
+
 
         private void NextPage_Event(object sender, EventArgs e)
         {
@@ -242,8 +311,8 @@ namespace Reader
 
         private void FilePickerT_Event(object sender, EventArgs e)
         {
-            File_Picker FilePickerWindow = new File_Picker();
-            FilePickerWindow.Show();
+            //File_Picker FilePickerWindow = new File_Picker();
+            //FilePickerWindow.Show();
         }
 
         private void MainMenu_Event(object sender, EventArgs e)
@@ -253,11 +322,13 @@ namespace Reader
             {
                 MainMenu.Opacity = 0;
                 StackMenu.Visibility = Visibility.Collapsed;
+                MenuGrid.Visibility = Visibility.Collapsed;
             }
             else if (MainMenu.Opacity == 0)
             {
                 MainMenu.Opacity = 0.75;
                 StackMenu.Visibility = Visibility.Visible;
+                MenuGrid.Visibility = Visibility.Visible;
             }
 
         }
@@ -272,6 +343,31 @@ namespace Reader
         private void Quit_Event(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void MenuPanelbtn_Event(object sender, RoutedEventArgs e)
+        {
+            Button bn = sender as Button;
+            string Command = bn.Tag.ToString();
+            if (Command == "Explorer")
+            {
+                ShowExplorer();
+            }
+            else if (Command == "Reader")
+            {
+                ShowReader();
+            }
+            else if (Command == "Setting")
+            {
+                ShowSetting();
+            }
+            else
+            {
+                ShowExplorer();
+                GetContent(bn.Tag.ToString());
+                FilePickerT.ItemsSource = Items;
+                FilePickerT.Items.Refresh();
+            }
         }
     }
     
